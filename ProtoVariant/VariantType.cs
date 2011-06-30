7b3b5773207876fec444d5c82089c27b661de77c
Pieter van Ginkel <pvginkel@gmail.com>
2011-06-30 07:40:02 +0200

    * Added missing attributes on VariantType enum values.

--- ProtoVariant/VariantType.cs
+++ ProtoVariant/VariantType.cs
@@ -8,14 +8,25 @@ namespace ProtoVariant
     [ProtoContract]
     internal enum VariantType
     {
-        Auto,
-        BoolFalse,
-        BoolTrue,
-        Int32Zero,
-        Int64Zero,
-        FloatZero,
-        DoubleZero,
-        StringEmpty,
-        Null
+        [ProtoEnum(Value = 0)]
+        Auto = 0,
+        [ProtoEnum(Value = 1)]
+        Null = 1,
+        [ProtoEnum(Value = 2)]
+        BoolFalse = 2,
+        [ProtoEnum(Value = 3)]
+        BoolTrue = 3,
+        [ProtoEnum(Value = 4)]
+        Int32Zero = 4,
+        [ProtoEnum(Value = 5)]
+        Int64Zero = 5,
+        [ProtoEnum(Value = 6)]
+        FloatZero = 6,
+        [ProtoEnum(Value = 7)]
+        DoubleZero = 7,
+        [ProtoEnum(Value = 8)]
+        DateTimeZero = 8,
+        [ProtoEnum(Value = 9)]
+        StringEmpty = 9
     }
 }
